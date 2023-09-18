cfa16854647424f9861fb59d75633510d4fe29aa
Milagros <miilyailen97@gmail.com>
2023-09-18 10:41:18 -0300

    agrega archivo app

---
+++ app.js
@@ -0,0 +1,20 @@
+//todo:hacer el front de la calculadora
+const process = require("process");
+const { argv } = process;
+
+function calculadora(comando) {
+  switch (comando) {
+    case "sumar":
+      break;
+    case "restar":
+      break;
+    case "multiplicar":
+      break;
+    case "dividir":
+      break;
+
+    default:
+      "comando no valido";
+      break;
+  }
+}
